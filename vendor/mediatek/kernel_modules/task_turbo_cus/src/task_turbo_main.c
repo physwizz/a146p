f233ca253b7e552ac77f80f61c2f735bc159b651
physwizz <pollockg86@gmail.com>
2026-09-07 13:41:28 +1000

    Upgrade to V UI

---
+++ vendor/mediatek/kernel_modules/task_turbo_cus/src/task_turbo_main.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2023 MediaTek Inc.
+ */
+
+#include <linux/module.h>
+
+#include "task_turbo_ko.h"
+
+#line __LINE__ "vendor/mediatek/kernel_modules/task_turbo_int/src/task_turbo_main.c"
+
+static void __exit task_turbo_v_exit(void) {}
+
+static int __init task_turbo_v_init(void)
+{
+	init_task_turbo_hook();
+	pr_info("%s %d: task turbo hook done", __func__, __LINE__);
+	return 0;
+}
+bool get_cam_status_for_task_turbo(void)
+{
+    return 0;
+}
+
+int get_cam_hal_pid_for_task_turbo(void)
+{
+    return -1;
+}
+
+int get_cam_server_pid_for_task_turbo(void)
+{
+    return -1;
+} 
+module_init(task_turbo_v_init);
+module_exit(task_turbo_v_exit);
+
+MODULE_LICENSE("Proprietary");
+MODULE_DESCRIPTION("MediaTek task_turbo_v");
+MODULE_AUTHOR("MediaTek Inc.");
